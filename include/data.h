e42afe339fde0fdf26e3a1468b377f24f614498f
laushkin1 <laushkinivan1@gmail.com>
2025-05-17 06:48:48 +0200

    feat: add gui

--- include/data.h
+++ include/data.h
@@ -1,12 +1,17 @@
 #ifndef DATA_H
 #define DATA_H
 
-#define NUMBER_OF_ITEMS 16
-#define MAX_NAME 100
-#define MIN_ARMY 1          // minimum number of units in the army
-#define MAX_ARMY 5          // maximum number of units in the army
+#define NUMBER_OF_ITEMS 16  // number of available items in the database
+#define NUMBER_OF_UNITS 16  // number of available units in the database
+#define MAX_NAME 100        // maximum length of item or unit name
+#define MIN_ARMY 1          // minimum number of combat units in an army
+#define MAX_ARMY 5          // maximum number of combat units in an army
+#define FIELD_ROWS 5        // number of battlefield rows
+#define FIELD_COLS 5        // number of battlefield cols
+#include <stdbool.h>
 
 
+// Structure representing an item in inventory
 typedef struct item {
     char name[MAX_NAME + 1];
     int att;
@@ -16,15 +21,38 @@ typedef struct item {
     int radius;
 } ITEM;
 
+// Structure representing a combat unit
 typedef struct unit {
     char name[MAX_NAME + 1];
     const ITEM *item1;
     const ITEM *item2;
     int hp;
+    const char *description;
 } UNIT;
 
+// Structure representing a window dimensions
+typedef struct {
+    int x;
+    int y;
+    int width;
+    int height;
+} WindowDimensions;
+
+// Structure representing a menu item (button)
+typedef struct {
+    char *label;
+    int selected;
+} MenuItem;
+
+// Structure representing a selectable unit in a menu
+typedef struct {
+    const UNIT* unit;
+    bool selected;
+    bool available;
+} MENU_UNIT;
+
+// Database of items available to combat units
 extern const ITEM items[NUMBER_OF_ITEMS];
+extern const UNIT units[NUMBER_OF_UNITS];
 
-#endif //DATA_H
-
-
+#endif
--- src/data.c
+++ src/data.c
@@ -1,4 +1,5 @@
 #include "data.h"
+#include <stddef.h>
 
 const ITEM items[NUMBER_OF_ITEMS] = {
         {.name = "wand", .att = 12, .def = 4, .slots = 1, .range = 4, .radius = 2}, //                  0
@@ -18,4 +19,119 @@ const ITEM items[NUMBER_OF_ITEMS] = {
         {.name = "hammer", .att = 8, .def = 2, .slots = 2, .range = 1, .radius = 2}, //                 13
         {.name = "crossbow", .att = 5, .def = 1, .slots = 2, .range = 3, .radius = 0}, //               14
         {.name = "slingshot", .att = 2, .def = 0, .slots = 2, .range = 2, .radius = 1} //               15
+};
+
+const UNIT units[NUMBER_OF_UNITS] = {
+    {
+        .name = "Pyromancer",
+        .item1 = &items[0],     // wand
+        .item2 = &items[1],     // fireball
+        .hp = 60,
+        .description = "A fiery spellcaster combining pinpoint attacks and area damage."
+    },
+    {
+        .name = "Warrior",
+        .item1 = &items[2],     // sword
+        .item2 = &items[6],     // armor
+        .hp = 85,
+        .description = "A melee fighter with balanced attack and solid defense."
+    },
+    {
+        .name = "Vanguard",
+        .item1 = &items[3],     // spear
+        .item2 = &items[9],     // helmet
+        .hp = 75,
+        .description = "A frontline soldier who keeps enemies at bay while staying protected."
+    },
+    {
+        .name = "Assassin",
+        .item1 = &items[4],     // dagger
+        .item2 = &items[8],     // gloves
+        .hp = 55,
+        .description = "A fast and stealthy killer that strikes swiftly in close combat."
+    },
+    {
+        .name = "Stonecaller",
+        .item1 = &items[5],     // rock
+        .item2 = &items[10],    // aura
+        .hp = 70,
+        .description = "A mystical unit using natural forces and protective energy."
+    },
+    {
+        .name = "Guardian",
+        .item1 = &items[7],     // shield
+        .item2 = &items[9],     // helmet
+        .hp = 90,
+        .description = "A heavily armored unit designed to absorb damage and protect allies."
+    },
+    {
+        .name = "HeavyGunner",
+        .item1 = &items[11],    // cannon
+        .item2 = NULL,
+        .hp = 80,
+        .description = "An artillery expert delivering massive area damage from afar."
+    },
+    {
+        .name = "Berserker",
+        .item1 = &items[12],    // axe
+        .item2 = NULL,
+        .hp = 85,
+        .description = "A savage fighter wielding a brutal weapon for devastating attacks."
+    },
+    {
+        .name = "Crusader",
+        .item1 = &items[13],    // hammer
+        .item2 = NULL,
+        .hp = 88,
+        .description = "A holy warrior using a mighty hammer to crush foes."
+    },
+    {
+        .name = "Sharpshooter",
+        .item1 = &items[14],    // crossbow
+        .item2 = NULL,
+        .hp = 65,
+        .description = "A precise ranged attacker effective from a safe distance."
+    },
+    {
+        .name = "Trickster",
+        .item1 = &items[15],    // slingshot
+        .item2 = NULL,
+        .hp = 60,
+        .description = "A nimble fighter using improvised ranged tactics to harass enemies."
+    },
+    {
+        .name = "Templar",
+        .item1 = &items[0],     // wand
+        .item2 = &items[10],    // aura
+        .hp = 70,
+        .description = "A divine caster with both offensive and defensive magic."
+    },
+    {
+        .name = "Skirmisher",
+        .item1 = &items[3],     // spear
+        .item2 = &items[5],     // rock
+        .hp = 68,
+        .description = "An agile fighter combining medium-range thrusts with throwable rocks."
+    },
+    {
+        .name = "Juggernaut",
+        .item1 = &items[6],     // armor
+        .item2 = &items[7],     // shield
+        .hp = 100,
+        .description = "An unstoppable defensive wall, ideal for absorbing damage."
+    },
+    {
+        .name = "BladeDancer",
+        .item1 = &items[2],     // sword
+        .item2 = &items[8],     // gloves
+        .hp = 70,
+        .description = "A fast and graceful swordsman focused on finesse and precision."
+    },
+    {
+        .name = "Raider",
+        .item1 = &items[12],    // axe
+        .item2 = &items[4],     // dagger
+        .hp = 80,
+        .description = "A ruthless attacker using brutal melee combinations."
+    }
 };
